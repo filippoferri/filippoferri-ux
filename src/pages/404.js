c0ce4a5d45def3249cc509a7225e85f4030a3c80
Filippo Ferri <hello@filippoferri.co>
2019-04-05 09:21:25 +0200

    added 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
+
+import error from "../img/svg/404.svg";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <section className="section">
+      <div className="columns is-multiline">
+        <div className="container content">
+          <div className="column has-text-centered">
+            <img src={error} alt="404"/>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
